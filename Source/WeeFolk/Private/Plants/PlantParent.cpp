c3f98d44817321dd66c339580338555b36d64fb7
Jonathan Tee <1900172@uad.ac.uk>
2024-07-24 19:20:29 +0100

    Made parent for plant class

---
+++ Source/WeeFolk/Private/Plants/PlantParent.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Plants/PlantParent.h"
+
+// Sets default values
+APlantParent::APlantParent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlantParent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlantParent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APlantParent::IncrementGrowthTimer(const float DeltaTime, float& timer)
+{
+	// Incremtent the timer and output new value
+	GrowthTimer += DeltaTime;
+	timer = GrowthTimer;
+
+	// If due growth grow
+	if (GrowthTimer >= GrowthCycleLength)
+		Growth();
+}
+
+void APlantParent::Growth()
+{
+	// TODO : Add if for using soil quality
+
+	GrowthTimer -= GrowthCycleLength;
+	cycleCounter++;
+
+	// TODO : Add if for spreading
+}
---
+++ Source/WeeFolk/Public/Plants/PlantParent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlantParent.generated.h"
+
+UCLASS()
+class WEEFOLK_API APlantParent : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlantParent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Variables
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Timers")
+	float GrowthTimer;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Timers")
+	float GrowthCycleLength;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Timers")
+	bool isFullyGrown;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Timers")
+	int32 MiniumCyclesPerSpread;
+
+	int32 cycleCounter;
+
+	// Functions
+	void Growth();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Variables
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Visual Component")
+	UStaticMesh* VisualComponent;
+
+	// Functions
+	UFUNCTION(BlueprintCallable, Category = "Timers")
+	void IncrementGrowthTimer(const float DeltaTime, float& timer);
+};
